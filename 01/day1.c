ed4ecf03a40eb8aed884e6c811e2cc383b057ee9
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-09 13:12:01 +0100

    add solution to part 1 of day 1

--- 01/day1.c
+++ 01/day1.c
@@ -1,38 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../file.c"
 
-#define BUFFER_SIZE 1024
-
-int read_file_to_array(long *array)
+long count_increments(char *buffer)
 {
-    char *filename = "sample.txt";
-    FILE *fp = fopen(filename, "r");
+    char *end = buffer;
 
-    if (fp == NULL)
+    long prev, next, cnt = 0;
+    next = prev = strtol(buffer, &end, 10);
+
+    while (*end != 0 && next != 0)
     {
-        printf("Error: could not open file %s\n", filename);
-        return -1;
-    }
-
-    fclose(fp);
-
-    return 0;
-}
-
-long count_increments(long *array)
-{
-    char input[] = "199 200 208 210 200 207 240 269 260 263";
-    char *ptr_end = input, *ptr_start = input;
-
-    long next, prev, cnt = 0;
-    prev = strtol(ptr_start, &ptr_end, 10);
-
-    while (*ptr_end != 0)
-    {
-        next = strtol(ptr_start, &ptr_end, 10);
+        next = strtol(buffer, &end, 10);
         if (next > prev) ++cnt;
         prev = next;
-        ptr_start = ptr_end;
+        buffer = end;
     }
 
     return cnt;
@@ -40,12 +22,13 @@ long count_increments(long *array)
 
 int main()
 {
-    long input[BUFFER_SIZE] = {0};
-    read_file_to_array(input);
+    char *buffer = read_file("input.txt");
 
     printf("Day 1!\n");
-    printf("* pt. 1: %ld\n", count_increments(input));
+    printf("* pt. 1: %ld\n", count_increments(buffer));
     // printf("* pt. 2: %ld\n", count_increments(fp));
+
+    free(buffer);
 
     return 0;
 }
