27872519126a81f272d118d551a62a14e8290ab7
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-09 13:58:57 +0100

    add solution to part 2 of day 1

--- 01/day1.c
+++ 01/day1.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../file.c"
+#include "../array.c"
 
-long count_increments(char *buffer)
+long count_increments(char *buffer, size_t n)
 {
     char *end = buffer;
+    long frame[n];
+    long prev_sum, next_sum = 0, cnt = 0;
 
-    long prev, next, cnt = 0;
-    next = prev = strtol(buffer, &end, 10);
-
-    while (*end != 0 && next != 0)
+    for (size_t i = 0; i < n; ++i)
     {
-        next = strtol(buffer, &end, 10);
-        if (next > prev) ++cnt;
-        prev = next;
+        frame[i] = strtol(buffer, &end, 10);
+        buffer = end;
+    }
+
+    prev_sum = sum_array(frame, n);
+
+    while (*end != 0 && frame[n - 1] != 0)
+    {
+        for (size_t i = 0; i < n - 1; ++i)
+        {
+            frame[i] = frame[i + 1];
+        }
+        frame[n - 1] = strtol(buffer, &end, 10);
+        next_sum = sum_array(frame, n);
+        if (next_sum > prev_sum) ++cnt;
+        prev_sum = next_sum;
         buffer = end;
     }
 
@@ -25,8 +38,8 @@ int main()
     char *buffer = read_file("input.txt");
 
     printf("Day 1!\n");
-    printf("* pt. 1: %ld\n", count_increments(buffer));
-    // printf("* pt. 2: %ld\n", count_increments(fp));
+    printf("* pt. 1: %ld\n", count_increments(buffer, 1));
+    printf("* pt. 2: %ld\n", count_increments(buffer, 3));
 
     free(buffer);
 
