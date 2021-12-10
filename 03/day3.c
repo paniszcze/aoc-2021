c52efaaece555800d29e23f2bc72d29a161c246a
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-10 22:35:02 +0100

    add solution to part 2 of day 3

--- 03/day3.c
+++ 03/day3.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include "../file.c"
 
-#define BIT_COUNT 5
+#define BIT_COUNT 12
 
 long read_power_consumption(long *numbers, size_t count)
 {
@@ -43,51 +43,69 @@ long read_life_support(long *numbers, size_t count)
 
     long oxygen;
     size_t records_left_oxy = count;
-    printf("\ncount %lu %lu\n", records_left_oxy, records_left_oxy >> 1);
     for (size_t i = 0; i < BIT_COUNT; ++i)
     {
         long bit_sum_oxy = 0;
         for (size_t j = 0; j < count; ++j)
         {
             if (!records[j].discarded_oxy)
-            {
-                bit_sum_oxy += records[j].value >> (BIT_COUNT - i - 1) & 1;
-                printf("%lu, ", records[j].value);
-            }
+                bit_sum_oxy += (records[j].value >> (BIT_COUNT - i - 1)) & 1;
         }
         long bit_oxy = 0;
         if (bit_sum_oxy > (records_left_oxy >> 1)) bit_oxy = 1;
-        else if (bit_sum_oxy = (records_left_oxy >> 1)) bit_oxy = 1;
-        printf("\n%lu:%ld left %lu\n", bit_sum_oxy, bit_oxy, records_left_oxy);
+        else if (!(records_left_oxy & 1) && (bit_sum_oxy == (records_left_oxy >> 1))) bit_oxy = 1;
         for (size_t j = 0; j < count; ++j)
         {
             if (!records[j].discarded_oxy &&
-                ((records[j].value >> (BIT_COUNT - i - 1)) & 1 == bit_oxy))
+                (((records[j].value >> (BIT_COUNT - i - 1)) & 1) == bit_oxy))
             {
                 oxygen = records[j].value;
-                printf("oxy %lu, ", oxygen);
             }
             else if (!records[j].discarded_oxy)
             {
                 records[j].discarded_oxy = true;
                 --records_left_oxy;
-                printf("ile %lu, ", records_left_oxy);
             }
         }
-        printf("\n");
         if (records_left_oxy == 1) break;
     }
 
-    long co2 = 1;
+    long co2;
+    size_t records_left_co2 = count;
+    for (size_t i = 0; i < BIT_COUNT; ++i)
+    {
+        long bit_sum = 0;
+        for (size_t j = 0; j < count; ++j)
+        {
+            if (!records[j].discarded_co2)
+                bit_sum += (records[j].value >> (BIT_COUNT - i - 1)) & 1;
+        }
+        long bit = 1;
+        if (bit_sum > (records_left_co2 >> 1)) bit = 0;
+        else if (!(records_left_co2 & 1) && (bit_sum == (records_left_co2 >> 1))) bit = 0;
+        for (size_t j = 0; j < count; ++j)
+        {
+            if (!records[j].discarded_co2 &&
+                (((records[j].value >> (BIT_COUNT - i - 1)) & 1) == bit))
+            {
+                co2 = records[j].value;
+            }
+            else if (!records[j].discarded_co2)
+            {
+                records[j].discarded_co2 = true;
+                --records_left_co2;
+            }
+        }
+        if (records_left_co2 == 1) break;
+    }
 
-    printf("\n%lu %lu\n", oxygen, co2);
     return oxygen * co2;
 }
 
 int main()
 {
     size_t count = 0;
-    long *numbers = read_numbers_from_file("sample.txt", &count, 2);
+    long *numbers = read_numbers_from_file("input.txt", &count, 2);
 
     printf("Day 3!\n");
     printf("* pt. 1: %ld\n", read_power_consumption(numbers, count));
