d388bbc3aa107e9175eab6113d92daad9066ac6c
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-10 09:19:26 +0100

    add solution to part 1 of day 2

--- 02/day2.c
+++ 02/day2.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "../file.c"
 
 long follow_instructions(char *buffer)
 {
-    long position = 0, depth = 0;
-
+    long value = -1, position = 0, depth = 0;
     char *end = buffer;
+    char command;
+
+    while (*end != 0 && value != 0)
+    {
+        while (isspace(*buffer)) ++buffer;
+        command = *buffer;
+
+        while (isalpha(*buffer)) ++buffer;
+        value = strtol(buffer, &end, 10);
+        buffer = end;
+
+        if (command == 'f')
+            position += value;
+        else if (command == 'u')
+            depth -= value;
+        else if (command == 'd')
+            depth += value;
+    }
 
     return position * depth;
 }
