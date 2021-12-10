4a28f82f29a43252ea7b923d6eccd00b9120ff9b
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-10 09:35:56 +0100

    add solution to part 2 of day 2

--- 02/day2.c
+++ 02/day2.c
@@ -3,9 +3,12 @@
 #include <ctype.h>
 #include "../file.c"
 
-long follow_instructions(char *buffer)
+#define PART1 0
+#define PART2 1
+
+long follow_instructions(char *buffer, int part)
 {
-    long value = -1, position = 0, depth = 0;
+    long value = -1, position = 0, depth = 0, aim = 0;
     char *end = buffer;
     char command;
 
@@ -18,12 +21,25 @@ long follow_instructions(char *buffer)
         value = strtol(buffer, &end, 10);
         buffer = end;
 
-        if (command == 'f')
-            position += value;
-        else if (command == 'u')
-            depth -= value;
-        else if (command == 'd')
-            depth += value;
+        if (part == PART1)
+        {
+            if (command == 'f')
+            {
+                position += value;
+            }
+            else if (command == 'u') depth -= value;
+            else if (command == 'd') depth += value;
+        }
+        else
+        {
+            if (command == 'f')
+            {
+                position += value;
+                depth += value * aim;
+            }
+            else if (command == 'u') aim -= value;
+            else if (command == 'd') aim += value;
+        }
     }
 
     return position * depth;
@@ -34,8 +50,8 @@ int main()
     char *buffer = read_file("input.txt");
 
     printf("Day 2!\n");
-    printf("* pt. 1: %ld\n", follow_instructions(buffer));
-    // printf("* pt. 2: %ld\n", follow_instructions(buffer));
+    printf("* pt. 1: %ld\n", follow_instructions(buffer, PART1));
+    printf("* pt. 2: %ld\n", follow_instructions(buffer, PART2));
 
     free(buffer);
 
