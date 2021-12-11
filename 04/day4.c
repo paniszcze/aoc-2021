1b6e4c3dd90d4bad7ec19b49d6ec754fdbc8de25
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-11 12:15:34 +0100

    add solution to part 1 of day 4

--- 04/day4.c
+++ 04/day4.c
@@ -5,7 +5,10 @@
 #include "../file.c"
 
 #define BOARD_SIZE 5
-#define MAX_COUNT 100
+#define MAX_COUNT 128
+
+#define WIN 1
+#define LOOSE 0
 
 typedef struct Number
 {
@@ -16,16 +19,17 @@ typedef struct Number
 typedef struct Board
 {
     Number number[BOARD_SIZE][BOARD_SIZE];
+    bool won;
 } Board;
 
-void print_board(Board board)
+void print_board(Board *board)
 {
     for (size_t row = 0; row < BOARD_SIZE; ++row)
     {
         for (size_t col = 0; col < BOARD_SIZE; ++col)
         {
-            if (!board.number[row][col].marked)
-                printf("%2ld ", board.number[row][col].value);
+            if (!board->number[row][col].marked)
+                printf("%2ld ", board->number[row][col].value);
             else
                 printf(" x ");
         }
@@ -33,7 +37,65 @@ void print_board(Board board)
     }
 }
 
-long bingo(char *buffer)
+int mark_number(Board *board, long number)
+{
+    for (size_t row = 0; row < BOARD_SIZE; ++row)
+    {
+        for (size_t col = 0; col < BOARD_SIZE; ++col)
+        {
+            if (board->number[row][col].value == number)
+            {
+                board->number[row][col].marked = true;
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+bool check_win(Board *board)
+{
+    for (size_t row = 0; row < BOARD_SIZE; ++row)
+    {
+        size_t sum_marked_row = 0;
+        size_t sum_marked_col = 0;
+        for (size_t col = 0; col < BOARD_SIZE; ++col)
+        {
+            if (board->number[row][col].marked == true)
+                ++sum_marked_col;
+            if (sum_marked_col == BOARD_SIZE)
+            {
+                board->won = true;
+                return board->won;
+            }
+
+            if (board->number[col][row].marked == true)
+                ++sum_marked_row;
+        }
+        if (sum_marked_row == BOARD_SIZE)
+        {
+            board->won = true;
+            return board->won;
+        }
+    }
+    return board->won;
+}
+
+long sum_unmarked(Board *board)
+{
+    long sum = 0;
+    for (size_t row = 0; row < BOARD_SIZE; ++row)
+    {
+        for (size_t col = 0; col < BOARD_SIZE; ++col)
+        {
+            if (board->number[row][col].marked == false)
+                sum += board->number[row][col].value;
+        }
+    }
+    return sum;
+}
+
+long bingo(char *buffer, int mode)
 {
     char *end = buffer;
     long numbers[MAX_COUNT];
@@ -44,7 +106,8 @@ long bingo(char *buffer)
         numbers[nums_count] = strtol(buffer, &end, 10);
         buffer = end;
         ++buffer;
-        ++nums_count;
+        if (*buffer != 0)
+            ++nums_count;
     }
 
     Board boards[MAX_COUNT];
@@ -58,19 +121,29 @@ long bingo(char *buffer)
                 while (isspace(*buffer)) ++buffer;
                 boards[boards_count].number[i][j].value = strtol(buffer, &end, 10);
                 boards[boards_count].number[i][j].marked = false;
+                boards[boards_count].won = false;
                 buffer = end;
             }
         }
-        ++boards_count;
+        if (*buffer != 0)
+            ++boards_count;
     }
 
     for (size_t i = 0; i < nums_count; ++i)
     {
-        printf("%ld ", numbers[i]);
+        for (size_t j = 0; j < boards_count; ++j)
+        {
+            if (mode)
+            {
+                if (mark_number(&boards[j], numbers[i]) && check_win(&boards[j]))
+                    return sum_unmarked(&boards[j]) * numbers[i];
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
-    printf("\n");
-
-    print_board(boards[2]);
 
     return 0;
 }
@@ -80,8 +153,8 @@ int main()
     char *buffer = read_file("input.txt");
 
     printf("Day 4!\n");
-    printf("* pt. 1: %ld\n", bingo(buffer));
-    // printf("* pt. 2: %ld\n", bingo(buffer));
+    printf("* pt. 1: %ld\n", bingo(buffer, WIN));
+    // printf("* pt. 2: %ld\n", bingo(buffer. LOOSE));
 
     free(buffer);
 
