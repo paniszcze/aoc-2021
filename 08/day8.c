bd61f98d12480a144d1bcd11dde344544dcfead7
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-13 12:50:21 +0100

    add solution to part 1 of day 8

--- 08/day8.c
+++ 08/day8.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../file.c"
+#include "../strings.c"
 
-long find_lowest_fuel_consumption(char *buffer)
+// Segments pattern: 0b0absdefg
+// Digits with unique segments count:
+#define ONE     0b00010010  // 2
+#define FOUR    0b00111010  // 4
+#define SEVEN   0b01010010  // 3
+#define EIGHT   0b01111111  // 7
+
+// Five-segment digits:
+#define TWO     0b01011101
+#define THREE   0b01011011
+#define FIVE    0b01101011
+
+// Six-segment digits:
+#define ZERO    0b01110111
+#define SIX     0b01101111
+#define NINE    0b01111011
+
+size_t count_unique(char *buffer)
 {
-    return 0;
+    size_t len, count = 0;
+    char *begin = buffer, *end = buffer;
+
+    while (*begin != 0)
+    {
+        begin = strchr(begin, '|');
+        while (*begin != '\n' && *begin != 0)
+        {
+            begin = find_letter(begin);
+            end = find_space(begin);
+            len = end - begin;
+            if (len == 2 || len == 3 || len == 4 || len == 7)
+                ++count;
+            begin = end;
+        }
+    }
+
+    return count;
 }
 
 int main()
@@ -12,7 +48,7 @@ int main()
     char *buffer = read_file("input.txt");
 
     printf("Day 8!\n");
-    printf("* pt. 1: %ld\n", count_unique(buffer));
+    printf("* pt. 1: %lu\n", count_unique(buffer));
 
     free(buffer);
 
--- strings.c
+++ strings.c
@@ -1,11 +1,29 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+typedef struct Word
+{
+    char *start;
+    size_t size;
+} Word;
+
 char *find_digit(char *ptr)
 {
     while (!isdigit(*ptr) && *ptr != 0)
-    {
         ++ptr;
-    }
+    return ptr;
+}
+
+char *find_letter(char *ptr)
+{
+    while (!isalpha(*ptr) && *ptr != 0)
+        ++ptr;
+    return ptr;
+}
+
+char *find_space(char *ptr)
+{
+    while (!isspace(*ptr) && *ptr != 0)
+        ++ptr;
     return ptr;
 }
