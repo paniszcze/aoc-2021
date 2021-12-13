8060e06ec60d26ebe74a72621d426793c73e6603
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-13 15:49:24 +0100

    add solution to part 2 of day 8

--- 08/day8.c
+++ 08/day8.c
@@ -4,6 +4,9 @@
 #include "../file.c"
 #include "../strings.c"
 
+#define DIGITS 10
+#define OUTPUT 4
+
 // Segments pattern: 0b0absdefg
 // Digits with unique segments count:
 #define ONE     0b00010010  // 2
@@ -21,10 +24,17 @@
 #define SIX     0b01101111
 #define NINE    0b01111011
 
+typedef struct Digit
+{
+    u_int8_t bitmask;
+    size_t segments;
+    long value;
+} Digit;
+
 size_t count_unique(char *buffer)
 {
-    size_t len, count = 0;
-    char *begin = buffer, *end = buffer;
+    size_t count = 0, len;
+    char *begin = buffer;
 
     while (*begin != 0)
     {
@@ -32,15 +42,125 @@ size_t count_unique(char *buffer)
         while (*begin != '\n' && *begin != 0)
         {
             begin = find_letter(begin);
-            end = find_space(begin);
-            len = end - begin;
+            len = find_space(begin) - begin;
             if (len == 2 || len == 3 || len == 4 || len == 7)
                 ++count;
-            begin = end;
+            begin += len;
         }
     }
 
     return count;
+}
+
+void set_bitmask(Digit *digit, char ch)
+{
+    if (ch == 'a')
+        digit->bitmask |= 0b01000000;
+    else if (ch == 'b')
+        digit->bitmask |= 0b00100000;
+    else if (ch == 'c')
+        digit->bitmask |= 0b00010000;
+    else if (ch == 'd')
+        digit->bitmask |= 0b00001000;
+    else if (ch == 'e')
+        digit->bitmask |= 0b00000100;
+    else if (ch == 'f')
+        digit->bitmask |= 0b00000010;
+    else if (ch == 'g')
+        digit->bitmask |= 0b00000001;
+}
+
+size_t count_set_bits(u_int8_t bitmask)
+{
+    long count = 0;
+    while (bitmask)
+    {
+        count += bitmask & 1;
+        bitmask >>= 1;
+    }
+    return count;
+}
+
+long sum_output(char *buffer)
+{
+    long sum = 0;
+
+    while (*buffer != 0)
+    {
+        Digit digits[DIGITS + OUTPUT] = {{0}};
+
+        // convert letter sequences to bitmasks and assign values to unique patterns
+        for (size_t i = 0; i < DIGITS + OUTPUT; ++i)
+        {
+            buffer = find_letter(buffer);
+            while (isalpha(*buffer))
+            {
+                set_bitmask(&digits[i], *buffer++);
+                digits[i].segments = count_set_bits(digits[i].bitmask);
+            }
+            if (digits[i].segments == 2)
+                digits[i].value = 1;
+            else if (digits[i].segments == 4)
+                digits[i].value = 4;
+            else if (digits[i].segments == 3)
+                digits[i].value = 7;
+            else if (digits[i].segments == 7)
+                digits[i].value = 8;
+            else
+                digits[i].value = -1;
+        }
+
+        u_int8_t subtrahend_4_7 = 0;
+        u_int8_t subtrahend_1 = 0;
+        for (size_t i = 0; i < DIGITS; ++i)
+        {
+            if (digits[i].value == 4)
+                subtrahend_4_7 |= digits[i].bitmask;
+            else if (digits[i].value == 7)
+                subtrahend_4_7 |= digits[i].bitmask;
+            else if (digits[i].value == 1)
+                subtrahend_1 |= digits[i].bitmask;
+        }
+        // find digit 9, 2
+        for (size_t i = 0; i < DIGITS + OUTPUT; ++i)
+        {
+            if ((digits[i].value == -1) &&
+                (digits[i].segments == 6) &&
+                (count_set_bits(digits[i].bitmask ^ subtrahend_4_7) == 1))
+            {
+                digits[i].value = 9;
+            }
+            if ((digits[i].value == -1) &&
+                (digits[i].segments == 5) &&
+                (count_set_bits(digits[i].bitmask ^ subtrahend_4_7) == 4))
+            {
+                digits[i].value = 2;
+            }
+        }
+        // find digit 3 and 5, 0 and 6
+        for (size_t i = 0; i < DIGITS + OUTPUT; ++i)
+        {
+            if ((digits[i].value == -1) && (digits[i].segments == 5))
+            {
+                if (count_set_bits(digits[i].bitmask ^ subtrahend_1) == 3)
+                    digits[i].value = 3;
+                else
+                    digits[i].value = 5;
+            }
+            if ((digits[i].value == -1) && (digits[i].segments == 6))
+            {
+                if (count_set_bits(digits[i].bitmask ^ subtrahend_1) == 4)
+                    digits[i].value = 0;
+                else
+                    digits[i].value = 6;
+            }
+        }
+
+        sum += digits[DIGITS].value * 1000 + digits[DIGITS + 1].value * 100 +
+               digits[DIGITS + 2].value * 10 + digits[DIGITS + 3].value;
+    }
+
+    return sum;
 }
 
 int main()
@@ -49,6 +169,7 @@ int main()
 
     printf("Day 8!\n");
     printf("* pt. 1: %lu\n", count_unique(buffer));
+    printf("* pt. 2: %lu\n", sum_output(buffer));
 
     free(buffer);
 
