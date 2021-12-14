e775f9925dfd08bac2c8ba006ea9c79eec5bf3a1
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-14 09:45:45 +0100

    add solution to part 2 of day 9

--- 09/day9.c
+++ 09/day9.c
@@ -5,65 +5,64 @@
 
 #define WIDTH 100
 #define HEIGHT 100
+#define DIRECTIONS 4
+
+#define BASINS_BUFFER 550
 
 bool is_lowpoint(long map[HEIGHT][WIDTH], size_t row, size_t col)
 {
-    if (row == 0)
+    long directions[DIRECTIONS][2] =
     {
-        if (col == 0)
-        {
-            return (map[row + 1][col] > map[row][col] && map[row][col + 1] > map[row][col]);
-        }
-        else if (col == WIDTH - 1)
-        {
-            return (map[row + 1][col] > map[row][col] && map[row][col - 1] > map[row][col]);
-        }
-        else
-        {
-            return (map[row + 1][col] > map[row][col] &&
-                    map[row][col + 1] > map[row][col] &&
-                    map[row][col - 1] > map[row][col]);
-        }
-    }
-    else if (row == HEIGHT - 1)
+        {-1,  0},
+        { 0, -1},
+        { 1,  0},
+        { 0,  1}
+    };
+
+    for (size_t i = 0; i < DIRECTIONS; ++i)
     {
-        if (col == 0)
-        {
-            return (map[row - 1][col] > map[row][col] && map[row][col + 1] > map[row][col]);
-        }
-        else if (col == WIDTH - 1)
-        {
-            return (map[row - 1][col] > map[row][col] && map[row][col - 1] > map[row][col]);
-        }
-        else
-        {
-            return (map[row - 1][col] > map[row][col] &&
-                    map[row][col + 1] > map[row][col] &&
-                    map[row][col - 1] > map[row][col]);
-        }
+        size_t n_row = row + directions[i][0];
+        size_t n_col = col + directions[i][1];
+        if (0 <= n_row && n_row < HEIGHT)
+            if (0 <= n_col && n_col < WIDTH)
+                if (map[n_row][n_col] <= map[row][col])
+                    return false;
     }
-    else
+
+    return true;
+}
+
+void explore_basin(long map[HEIGHT][WIDTH],
+                   bool visited[HEIGHT][WIDTH],
+                   size_t row, size_t col, size_t *size)
+{
+    if (visited[row][col] == true)
+        return;
+    visited[row][col] = true;
+    ++(*size);
+
+    long directions[DIRECTIONS][2] =
     {
-        if (col == 0)
-        {
-            return (map[row - 1][col] > map[row][col] &&
-                    map[row + 1][col] > map[row][col] &&
-                    map[row][col + 1] > map[row][col]);
-        }
-        else if (col == WIDTH - 1)
-        {
-            return (map[row - 1][col] > map[row][col] &&
-                    map[row + 1][col] > map[row][col] &&
-                    map[row][col - 1] > map[row][col]);
-        }
-        else
-        {
-            return (map[row - 1][col] > map[row][col] &&
-                    map[row + 1][col] > map[row][col] &&
-                    map[row][col - 1] > map[row][col] &&
-                    map[row][col + 1] > map[row][col]);
-        }
+        {-1,  0},
+        { 0, -1},
+        { 1,  0},
+        { 0,  1}
+    };
+
+    for (size_t i = 0; i < DIRECTIONS; ++i)
+    {
+        size_t n_row = row + directions[i][0];
+        size_t n_col = col + directions[i][1];
+        if (0 <= n_row && n_row < HEIGHT)
+            if (0 <= n_col && n_col < WIDTH)
+                if (map[n_row][n_col] >= map[row][col] && map[n_row][n_col] < 9)
+                    explore_basin(map, visited, n_row, n_col, size);
     }
+}
+
+int compare(const void *a, const void *b)
+{
+    return *(long *)b - *(long *)a;
 }
 
 long sum_risk_levels(char *buffer)
@@ -79,17 +78,40 @@ long sum_risk_levels(char *buffer)
     }
 
     for (size_t i = 0; i < HEIGHT; ++i)
-    {
         for (size_t j = 0; j < WIDTH; ++j)
-        {
             if (is_lowpoint(hightmap, i, j))
-            {
                 sum += hightmap[i][j] + 1;
-            }
-        }
-    }
 
     return sum;
+}
+
+long find_basins(char *buffer)
+{
+    long hightmap[HEIGHT][WIDTH] = {0};
+
+    for (size_t i = 0; i < HEIGHT; ++i)
+    {
+        for (size_t j = 0; j < WIDTH; ++j)
+            hightmap[i][j] = *buffer++ - '0';
+        ++buffer;
+    }
+
+    bool visited[HEIGHT][WIDTH] = {0};
+    long basins[BASINS_BUFFER] = {0};
+    size_t count = 0;
+
+    for (size_t i = 0; i < HEIGHT; ++i)
+        for (size_t j = 0; j < WIDTH; ++j)
+            if (is_lowpoint(hightmap, i, j))
+            {
+                size_t size = 0;
+                explore_basin(hightmap, visited, i, j, &size);
+                basins[count++] = size;
+            }
+
+    qsort(basins, count, sizeof(long), compare);
+
+    return basins[0] * basins[1] * basins[2];
 }
 
 int main()
@@ -98,7 +120,7 @@ int main()
 
     printf("Day 9!\n");
     printf("* pt. 1: %ld\n", sum_risk_levels(buffer));
-    // printf("* pt. 2: %ld\n", lava(buffer));
+    printf("* pt. 2: %ld\n", find_basins(buffer));
 
     free(buffer);
 
