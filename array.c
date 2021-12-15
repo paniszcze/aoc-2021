c10cb60e4b2c92dc6f60eeec33bbfedb9831aaf8
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-15 12:51:08 +0100

    add print array function

--- array.c
+++ array.c
@@ -9,3 +9,13 @@ long sum_array(long *array, size_t size)
     }
     return sum;
 }
+
+void print_array(size_t row, size_t col, long array[][col])
+{
+    for (size_t i = 0; i < row; ++i)
+    {
+        for (size_t j = 0; j < col; ++j)
+            printf("%ld", array[i][j]);
+        printf("\n");
+    }
+}
