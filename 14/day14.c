27cf43c68979ce15195d0a3fd17c52c36eea0344
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-15 15:23:18 +0100

    add ipnut data for days 13, 14 and 15

---
+++ 14/day14.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../file.c"
+
+long func(char *buffer)
+{
+    return 0;
+}
+
+int main()
+{
+    char *buffer = read_file("input.txt");
+
+    printf("Day 14!\n");
+    printf("* pt. 1: %ld\n", func(buffer));
+    // printf("* pt. 2: %ld\n", func(buffer));
+
+    free(buffer);
+
+    return 0;
+}
