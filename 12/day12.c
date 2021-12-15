1a878eac2384519947538e4e3098c894320a5cf5
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-15 16:43:24 +0100

    save WIP on day 12

--- 12/day12.c
+++ 12/day12.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include "../file.c"
 
-long find_passage(char *buffer)
+void create_graph(char *buffer)
 {
+
+}
+
+long count_possible_paths(char *buffer)
+{
+    create_graph(buffer);
+
+
     return 0;
 }
 
 int main()
 {
-    char *buffer = read_file("input.txt");
+    char *buffer = read_file("sample1.txt");
 
     printf("Day 12!\n");
-    printf("* pt. 1: %ld\n", find_passage(buffer));
+    printf("* pt. 1: %ld\n", count_possible_paths(buffer));
     // printf("* pt. 2: %ld\n", find_passage(buffer));
 
     free(buffer);
