a8cd1ea15befddd727c5de3a51c1b4395b2e8121
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-17 16:48:44 +0100

    save WIP on day 15

--- 15/day15.c
+++ 15/day15.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "../array.c"
 #include "../file.c"
 
-long func(char *buffer)
+#define WIDTH 10
+#define HEIGHT 10
+
+void get_input(long array[][WIDTH])
 {
+    char *buffer = read_file("sample.txt");
+    char *ptr = buffer;
+
+    for (size_t i = 0; i < HEIGHT; ++i)
+    {
+        for (size_t j = 0; j < WIDTH; ++j)
+            array[i][j] = *ptr++ - '0';
+        ++ptr;
+    }
+
+    free(buffer);
+}
+
+long func()
+{
+    long risk_map[HEIGHT][WIDTH] = {0};
+    get_input(risk_map);
+    
+    print_array(HEIGHT, WIDTH, risk_map);
+
+    bool visited[HEIGHT][WIDTH] = {0};
+
     return 0;
 }
 
 int main()
 {
-    char *buffer = read_file("input.txt");
-
     printf("Day 15!\n");
-    printf("* pt. 1: %ld\n", func(buffer));
+    printf("* pt. 1: %ld\n", func());
     // printf("* pt. 2: %ld\n", func(buffer));
-
-    free(buffer);
 
     return 0;
 }
