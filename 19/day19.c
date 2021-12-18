d06cec2389b7e93f8c4429bcc9449ce9b94811a8
Daniel Panski <daniel.panski@tietoevry.com>
2021-12-18 11:14:45 +0100

    add placeholder files for days 16-21

---
+++ 19/day19.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../file.c"
+
+size_t func(char *buffer)
+{
+    return 0;
+}
+
+int main()
+{
+    char *buffer = read_file("input.txt");
+
+    printf("Day 19!\n");
+    printf("* pt. 1: %lu\n", func(buffer));
+    // printf("* pt. 2: %lu\n", func(buffer));
+
+    free(buffer);
+
+    return 0;
+}
